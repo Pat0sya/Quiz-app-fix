c54bfda7f133b56f1e9e41879bfec4dbee36f0bd
Egor <egorskid@yandex.ru>
2024-04-13 20:29:11 +0300

    NOW WOTH DATABASE!

--- lib/views/create_quiz.dart
+++ lib/views/create_quiz.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutterapp/services/database.dart';
+import 'package:flutterapp/views/addquistion.dart';
 import 'package:flutterapp/widgets/widgets.dart';
 import 'package:random_string/random_string.dart';
 
@@ -15,10 +17,14 @@ class _CreateQuizState extends State<CreateQuiz> {
   final _formKey = GlobalKey<FormState>();
   late String quizImageUrl, quizTitle, quizDescripstion, quizId;
   DatabaseServie databaseServie = new DatabaseServie();
+  bool _isLoading = false;
 
-  CreateQuizOnLine() {
+  CreateQuizOnLine() async {
     if (_formKey.currentState!.validate()) {
       quizId = randomAlphaNumeric(16);
+      setState(() {
+        _isLoading = true;
+      });
 
       Map<String, String> quizMap = {
         "quizId": quizId,
@@ -26,7 +32,16 @@ class _CreateQuizState extends State<CreateQuiz> {
         "quizTitle": quizTitle,
         "quizDescription": quizDescripstion
       };
-      databaseServie.addQuizData(quizMap, quizId);
+
+      await databaseServie.addQuizData(quizMap, quizId).then((value) {
+        setState(() {
+          _isLoading = false;
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => AddQuestion()),
+          );
+        });
+      });
     }
   }
 
@@ -40,54 +55,66 @@ class _CreateQuizState extends State<CreateQuiz> {
         iconTheme: IconThemeData(color: Colors.black87),
         systemOverlayStyle: SystemUiOverlayStyle.dark,
       ),
-      body: Form(
-        key: _formKey,
-        child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              children: [
-                TextFormField(
-                  validator: (val) => val!.isEmpty ? "Enter Image URL" : null,
-                  decoration: InputDecoration(
-                    hintText: "Quiz Image Url",
-                  ),
-                  onChanged: (val) {
-                    quizImageUrl = val;
-                  },
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                TextFormField(
-                  validator: (val) => val!.isEmpty ? "Enter Quiz Title" : null,
-                  decoration: InputDecoration(
-                    hintText: "Quiz Title",
-                  ),
-                  onChanged: (val) {
-                    quizTitle = val;
-                  },
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                TextFormField(
-                  validator: (val) =>
-                      val!.isEmpty ? "Enter Quiz Description" : null,
-                  decoration: InputDecoration(
-                    hintText: "Enter Quiz Description",
-                  ),
-                  onChanged: (val) {
-                    quizDescripstion = val;
-                  },
-                ),
-                Spacer(),
-                blueButton(context, 'Create Quiz'),
-                SizedBox(
-                  height: 60,
-                ),
-              ],
-            )),
-      ),
+      body: _isLoading
+          ? Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : Form(
+              key: _formKey,
+              child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        validator: (val) =>
+                            val!.isEmpty ? "Enter Image URL" : null,
+                        decoration: InputDecoration(
+                          hintText: "Quiz Image Url",
+                        ),
+                        onChanged: (val) {
+                          quizImageUrl = val;
+                        },
+                      ),
+                      SizedBox(
+                        height: 6,
+                      ),
+                      TextFormField(
+                        validator: (val) =>
+                            val!.isEmpty ? "Enter Quiz Title" : null,
+                        decoration: InputDecoration(
+                          hintText: "Quiz Title",
+                        ),
+                        onChanged: (val) {
+                          quizTitle = val;
+                        },
+                      ),
+                      SizedBox(
+                        height: 6,
+                      ),
+                      TextFormField(
+                        validator: (val) =>
+                            val!.isEmpty ? "Enter Quiz Description" : null,
+                        decoration: InputDecoration(
+                          hintText: "Enter Quiz Description",
+                        ),
+                        onChanged: (val) {
+                          quizDescripstion = val;
+                        },
+                      ),
+                      Spacer(),
+                      GestureDetector(
+                          onTap: () {
+                            CreateQuizOnLine();
+                          },
+                          child: blueButton(context, 'Create Quiz')),
+                      SizedBox(
+                        height: 60,
+                      ),
+                    ],
+                  )),
+            ),
     );
   }
 }
